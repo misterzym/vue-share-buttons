60786b380708321dae37c0e120c5b9d27b680e83
artem <misterzym@mail.ru>
2021-03-25 09:46:11 +0300

    remove sass dependens

--- src/components/WhatsAppButton.vue
+++ src/components/WhatsAppButton.vue
@@ -41,12 +41,7 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-$main-color: hsla(142, 70%, 49%, 1);
-$focus-color: hsla(142, 64%, 74%, 0.4);
-$hover-color: hsla(142, 70%, 49%, 0.9);
-$painted-color: hsla(141, 47%, 39%, 1);
-
+<style lang="css" scoped>
 .share-button * {
   box-sizing: border-box;
 }
@@ -58,10 +53,8 @@ $painted-color: hsla(141, 47%, 39%, 1);
   padding: 10px 8px;
   margin: 4px;
   color: #fff;
-  background-color: $main-color;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-    "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
-    "Segoe UI Symbol";
+  background-color: #25d466;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   font-weight: 400;
   vertical-align: top;
   user-select: none;
@@ -74,186 +67,153 @@ $painted-color: hsla(141, 47%, 39%, 1);
   letter-spacing: normal;
   word-spacing: normal;
   text-shadow: none;
-  transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out,
-    border-color 0.3s ease-in-out;
-
-  &:disabled {
-    opacity: 0.9;
-  }
-
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 0 3px $focus-color;
-  }
-
-  &:hover {
-    background-color: $hover-color;
-  }
-
-  &:not(:disabled):not(.disabled) {
-    cursor: pointer;
-  }
-
-  &:last-child {
-    margin-right: 0;
-  }
-
-  &__icon {
-    display: inline-block;
-    padding: 0;
-    margin: 0 7px;
-    font-size: 0;
-    vertical-align: middle;
-
-    path {
-      fill: #fff;
-    }
-
-    &:last-child {
-      margin: 0;
-    }
-  }
-
-  &__text {
-    display: inline-block;
-    margin: 0 7px;
-    font-size: 16px;
-    vertical-align: middle;
-  }
-
-  &__counter {
-    display: inline-block;
-    padding: 3px 10px;
-    margin-left: 4px;
-    font-size: 12px;
-    border-left: 1px solid #fff;
-    vertical-align: middle;
-  }
-
-  &--circle {
-    min-width: 42px;
-    min-height: 42px;
-    padding: 10px;
-    border-radius: 42px;
-  }
-
-  &--outline {
-    background-color: transparent;
-    border: 1px solid;
-    background-color: transparent;
-    border-color: $main-color;
-
-    .share-button__text {
-      color: $main-color;
-    }
-
-    .share-button__icon path {
-      fill: $main-color;
-    }
-
-    .share-button__counter {
-      color: $hover-color;
-      border-color: $hover-color;
-    }
-
-    &:hover {
-      background-color: transparent;
-      border-color: $hover-color;
-
-      .share-button__text {
-        color: $main-color;
-      }
-
-      .share-button__icon path {
-        fill: $hover-color;
-      }
-    }
-  }
-
-  &--painted {
-    position: relative;
-    min-width: 42px;
-    min-height: 42px;
-    padding: 15px;
-    margin-bottom: 30px;
-    border-radius: 42px;
-    background-color: transparent;
-    border: 3px solid;
-    border-color: $painted-color;
-
-    &::before {
-      content: "";
-      z-index: -1;
-      position: absolute;
-      top: -1.5px;
-      left: -1.5px;
-      display: block;
-      width: calc(100% + 3px);
-      height: calc(100% + 3px);
-      background-color: $main-color;
-      border-radius: 50%;
-      transform: translate3d(3px, 2px, 0);
-      transition: transform 0.2s ease-in-out;
-    }
-
-    .share-button__icon {
-      width: 30px;
-      height: 30px;
-      margin: 0;
-    }
-
-    .share-button__text {
-      display: none;
-    }
-
-    .share-button__counter {
-      position: absolute;
-      bottom: -30px;
-      right: -7px;
-      margin: 0;
-      padding: 4px 10px;
-      border: 3px solid;
-      font-size: 8px;
-      border-radius: 15px;
-      color: #fff;
-      border-color: $painted-color;
-
-      &::before {
-        content: "";
-        z-index: -1;
-        position: absolute;
-        top: -1.65px;
-        left: -1.5px;
-        display: block;
-        width: calc(100% + 3px);
-        height: calc(100% + 3px);
-        border-radius: 15px;
-        transform: translate3d(-3px, 1.5px, 0);
-        transition: transform 0.2s ease-in-out;
-        background-color: $main-color;
-      }
-    }
-
-    &:hover {
-      &::before {
-        transform: translate3d(0, 0, 0);
-      }
-
-      .share-button__counter::before {
-        transform: translate3d(0px, 0px, 0);
-      }
-    }
-
-    &:focus {
-      &::before {
-        transform: translate3d(0, 0, 0);
-      }
-
-      .share-button__counter::before {
-        transform: translate3d(0px, 0px, 0);
-      }
-    }
-  }
+  transition: color 0.3s ease-in-out, background-color 0.3s ease-in-out, border-color 0.3s ease-in-out;
+}
+.share-button:disabled {
+  opacity: 0.9;
+}
+.share-button:focus {
+  outline: none;
+  box-shadow: 0 0 0 3px rgba(146, 231, 177, 0.4);
+}
+.share-button:hover {
+  background-color: rgba(37, 212, 102, 0.9);
+}
+.share-button:not(:disabled):not(.disabled) {
+  cursor: pointer;
+}
+.share-button:last-child {
+  margin-right: 0;
+}
+.share-button__icon {
+  display: inline-block;
+  padding: 0;
+  margin: 0 7px;
+  font-size: 0;
+  vertical-align: middle;
+}
+.share-button__icon path {
+  fill: #fff;
+}
+.share-button__icon:last-child {
+  margin: 0;
+}
+.share-button__text {
+  display: inline-block;
+  margin: 0 7px;
+  font-size: 16px;
+  vertical-align: middle;
+}
+.share-button__counter {
+  display: inline-block;
+  padding: 3px 10px;
+  margin-left: 4px;
+  font-size: 12px;
+  border-left: 1px solid #fff;
+  vertical-align: middle;
+}
+.share-button--circle {
+  min-width: 42px;
+  min-height: 42px;
+  padding: 10px;
+  border-radius: 42px;
+}
+.share-button--outline {
+  background-color: transparent;
+  border: 1px solid;
+  background-color: transparent;
+  border-color: #25d466;
+}
+.share-button--outline .share-button__text {
+  color: #25d466;
+}
+.share-button--outline .share-button__icon path {
+  fill: #25d466;
+}
+.share-button--outline .share-button__counter {
+  color: rgba(37, 212, 102, 0.9);
+  border-color: rgba(37, 212, 102, 0.9);
+}
+.share-button--outline:hover {
+  background-color: transparent;
+  border-color: rgba(37, 212, 102, 0.9);
+}
+.share-button--outline:hover .share-button__text {
+  color: #25d466;
+}
+.share-button--outline:hover .share-button__icon path {
+  fill: rgba(37, 212, 102, 0.9);
+}
+.share-button--painted {
+  position: relative;
+  min-width: 42px;
+  min-height: 42px;
+  padding: 15px;
+  margin-bottom: 30px;
+  border-radius: 42px;
+  background-color: transparent;
+  border: 3px solid;
+  border-color: #359255;
+}
+.share-button--painted::before {
+  content: "";
+  z-index: -1;
+  position: absolute;
+  top: -1.5px;
+  left: -1.5px;
+  display: block;
+  width: calc(100% + 3px);
+  height: calc(100% + 3px);
+  background-color: #25d466;
+  border-radius: 50%;
+  transform: translate3d(3px, 2px, 0);
+  transition: transform 0.2s ease-in-out;
+}
+.share-button--painted .share-button__icon {
+  width: 30px;
+  height: 30px;
+  margin: 0;
+}
+.share-button--painted .share-button__text {
+  display: none;
+}
+.share-button--painted .share-button__counter {
+  position: absolute;
+  bottom: -30px;
+  right: -7px;
+  margin: 0;
+  padding: 4px 10px;
+  border: 3px solid;
+  font-size: 8px;
+  border-radius: 15px;
+  color: #fff;
+  border-color: #359255;
+}
+.share-button--painted .share-button__counter::before {
+  content: "";
+  z-index: -1;
+  position: absolute;
+  top: -1.65px;
+  left: -1.5px;
+  display: block;
+  width: calc(100% + 3px);
+  height: calc(100% + 3px);
+  border-radius: 15px;
+  transform: translate3d(-3px, 1.5px, 0);
+  transition: transform 0.2s ease-in-out;
+  background-color: #25d466;
+}
+.share-button--painted:hover::before {
+  transform: translate3d(0, 0, 0);
+}
+.share-button--painted:hover .share-button__counter::before {
+  transform: translate3d(0px, 0px, 0);
+}
+.share-button--painted:focus::before {
+  transform: translate3d(0, 0, 0);
+}
+.share-button--painted:focus .share-button__counter::before {
+  transform: translate3d(0px, 0px, 0);
 }
 
 @media (max-width: 768px) {
@@ -262,46 +222,38 @@ $painted-color: hsla(141, 47%, 39%, 1);
     min-height: 38px;
     padding: 8px 8px;
     margin: 2px;
-
-    &__icon {
-      width: 18px;
-      height: 18px;
-      margin: 0 4px;
-    }
-
-    &__text {
-      margin: 0 4px;
-      font-size: 14px;
-    }
-
-    &--circle {
-      border-radius: 38px;
-    }
-
-    &--painted {
-      min-width: 48px;
-      min-height: 48px;
-      margin: 4px 4px 20px 4px;
-
-      &::before {
-        transform: translate3d(2.5px, 1.5px, 0);
-      }
-
-      .share-button__icon {
-        width: 20px;
-        height: 20px;
-      }
-
-      .share-button__counter {
-        bottom: -24px;
-        right: -8px;
-        padding: 2px 7px;
-
-        &::before {
-          transform: translate3d(-2px, 1.75px, 0);
-        }
-      }
-    }
+  }
+  .share-button__icon {
+    width: 18px;
+    height: 18px;
+    margin: 0 4px;
+  }
+  .share-button__text {
+    margin: 0 4px;
+    font-size: 14px;
+  }
+  .share-button--circle {
+    border-radius: 38px;
+  }
+  .share-button--painted {
+    min-width: 48px;
+    min-height: 48px;
+    margin: 4px 4px 20px 4px;
+  }
+  .share-button--painted::before {
+    transform: translate3d(2.5px, 1.5px, 0);
+  }
+  .share-button--painted .share-button__icon {
+    width: 20px;
+    height: 20px;
+  }
+  .share-button--painted .share-button__counter {
+    bottom: -24px;
+    right: -8px;
+    padding: 2px 7px;
+  }
+  .share-button--painted .share-button__counter::before {
+    transform: translate3d(-2px, 1.75px, 0);
   }
 }
 </style>
